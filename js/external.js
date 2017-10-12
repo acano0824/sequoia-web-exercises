87dfd327bb4fdd3d1cb541230a1c6459ec33f3a3
ashleycano <acano6835@gmail.com>
2017-10-12 16:11:50 -0500

    added javascript exercises

---
+++ js/external.js
@@ -0,0 +1,8 @@
+console.log('Hello from external JavaScript!');
+
+alert("Welcome to my website!");
+
+var userInput = prompt ('What is your favourite colour?');
+console.log('The user entered: ' + userInput);
+
+alert("Great, " + userInput + " is my favourite colour too!");
