ff202572e50dd08e8441c9df90d07da217049d04
ashleycano <acano6835@gmail.com>
2017-10-13 12:03:05 -0500

    added extra exercises to work on

--- js/functions.js
+++ js/functions.js
@@ -17,7 +17,7 @@
         return "Hello, " + name + "!";
     }
 
-    console.log(sayHello("codeup"));
+    // console.log(sayHello("codeup"));
 
 
     /**
@@ -31,7 +31,7 @@
 
     var helloMessage = sayHello("ashley");
 
-    console.log(helloMessage);
+    // console.log(helloMessage);
 
     /**
      * TODO:
@@ -42,7 +42,7 @@
 
     var myName = "ashley"
 
-    console.log(sayHello(myName));
+    // console.log(sayHello(myName));
 
 
 // Don't modify the following line, it generates a random number between 1 and 3
@@ -75,7 +75,7 @@
 
     var random = Math.floor((Math.random() * 3) + 1);
 
-    console.log(isTwo(random));
+    // console.log(isTwo(random));
 
 
     /**
@@ -105,20 +105,148 @@
      */
 
 
-    var total = prompt('How much was your bill?');
-    console.log('The user entered: ' + total);
+    // var total = prompt('How much was your bill?');
+    // console.log('The user entered: ' + total);
+    //
+    // var percent = prompt('What percentage would you like to tip?');
+    // console.log('The user entered: ' + percent);
+    //
+    // function calculateTip(percent, total) {
+    //     var result = percent * total;
+    //     return result;
+    // }
+    //
+    // var calculateTip = calculateTip(total, percent);
+    //
+    // alert("Great, you can tip $" + calculateTip);
 
-    var percent = prompt('What percentage would you like to tip?');
-    console.log('The user entered: ' + percent);
 
-    function calculateTip(percent, total) {
-        var result = percent * total;
-        return result;
+//Write a function called `identity(input)` that takes in an argument called
+// input and returns that input.
+
+function identity(input) {
+    return input;
     }
 
-    var calculateTip = calculateTip(total, percent);
 
-    alert("Great, you can tip $" + calculateTip);
+//Write a function called `getRandomNumber(min, max)` that returns a random number
+// between min and max values sent to that funciton call.
+
+
+
+
+
+
+//Write a function called `first(input)` that returns the first character in the provided string.
+
+
+
+
+
+//Write a fuction called `last(input)` that returns the last character of a string
+
+
+
+
+
+
+
+// Write a function called `rest(input)` that returns everything but the first character of a string.
+
+
+
+
+
+
+// Write a function called `reverse(input)` that takes a string and returns it reversed.
+
+
+
+
+
+
+
+//Write a function called `isNumeric(input)` that takes an input and
+// returns a boolean if the input is numeric.
+
+
+
+
+
+
+
+
+// Write a function called `count(input)` that takes in a string and returns the number of characters.
+
+
+
+
+
+
+// Write a function called `add(a, b)` that returns the sum of a and b
+
+
+
+
+
+
+// Write a function called `subtract(a, b)` that return the difference between the two inputs.
+
+
+
+
+
+
+// Write `multiply(a, b)` function that returns the product
+
+
+
+
+
+// Write a divide(numerator, denominator) function that returns a divided by b
+
+
+
+
+
+
+
+// Write a remainder(number, divisor) function that returns the remainder left
+// over when dividing `number` by the `divisor`
+
+
+
+
+
+
+// Write the function `square(a)` that takes in a number and returns the number multiplied by itself.
+
+
+
+
+
+
+
+
+// # Super Duper Gold-Star Bonus
+//Write a function called sumOfSquares(a, b) that uses only your add() function
+// and your square function and not + or * operators
+
+
+
+
+
+
+
+// Write a function called doMath(operator, a, b) that takes 3 parameters.
+// The first parameter is the name of the math function you want to apply. a and b are
+// the two numbers to run that function on.
+
+
+
+
+
+
 
 
 })();
