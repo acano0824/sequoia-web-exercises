b955d4dec72838ed4675940fd6659b1c3098d472
ashleycano <acano6835@gmail.com>
2017-10-19 14:15:03 -0500

    went over the walkthrough of for loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -10,7 +10,7 @@
 // }
 //     showMultiplicationTable(7);
 
-
+//
 // for (var i = 1; i <= 10; i++) {
 //     var random = Math.floor(Math.random() * 200) + 20;
 //     if (random % 2 === 0) {
@@ -21,20 +21,20 @@
 // }
 
 
-// for (var i = 1; i <= 9; i++) {
-//     var string = i.toString();
-//     var output = '';
-//     for (var j = 0; j < i; j++) {
-//         output += string;
-//     }
-//     console.log(output);
-// }
-
-
-
-for (var i = 100; i >= 5; i -= 5) {
-    console.log(i);
+for (var i = 1; i <= 9; i++) {
+    var string = i.toString();
+    var output = '';
+    for (var j = 0; j < i; j++) {
+        output += string;
+    }
+    console.log(output);
 }
+
+
+
+// for (var i = 100; i >= 5; i -= 5) {
+//     console.log(i);
+// }
 
 
 
